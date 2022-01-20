4a2430814bc90ca71dbf310ef1b8626dacba0670
Mario SAM <eu@mariosam.com.br>
2022-01-20 00:08:16 -0300

    Create MultiplicacaoSimples.js

---
+++ JS/MultiplicacaoSimples.js
@@ -0,0 +1,15 @@
+/**
+ * @version JAVASCRIPT
+ * @author MARIO SAM <eu@mariosam.com.br>
+ * @see I would love to work with you instead solving web code tests: hire me!
+ */
+
+//console.log("Result is "+getMulti(6, 2));
+
+/**
+ * Multiply two numbers.
+ */
+function getMulti(x, y) {
+    return x * y;
+}
+module.exports = {getMulti}
