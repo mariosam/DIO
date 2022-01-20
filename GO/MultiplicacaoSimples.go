92e00a8be55bac3ca2ab64e624ce364d4e9ebc2b
Mario SAM <eu@mariosam.com.br>
2022-01-20 00:05:40 -0300

    Create MultiplicacaoSimples.go

---
+++ GO/MultiplicacaoSimples.go
@@ -0,0 +1,13 @@
+/**
+ * @version GO
+ * @author MARIO SAM <eu@mariosam.com.br>
+ * @see I would love to work with you instead solving web code tests: hire me!
+ */
+package GO
+
+/**
+ * Multiply two numbers.
+ */
+func getMulti(x int, y int) int {
+	return x * y
+}
