e64411ff975e9c4da54cefcc663a06519177cfd5
Mario SAM <eu@mariosam.com.br>
2022-01-20 00:09:41 -0300

    Create MultiplicacaoSimples.php

---
+++ PHP/MultiplicacaoSimples.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @version PHP
+ * @author MARIO SAM <eu@mariosam.com.br>
+ * @see I would love to work with you instead solving web code tests: hire me!
+ */
+namespace PHP;
+
+//echo "The result is "+getMulti(4, 4, 6, 2);
+
+class MultiplicacaoSimples {
+
+    /**
+     * Multiply two numbers.
+     */
+    public function getMulti($x, $y): int {
+        return $x * $y;
+    }
+
+}
