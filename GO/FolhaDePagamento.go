5797f4ebce42ea8a4a781a2aae0687196e15eeec
Mario SAM <eu@mariosam.com.br>
2022-01-25 00:22:24 -0300

    Create FolhaDePagamento.go

---
+++ GO/FolhaDePagamento.go
@@ -0,0 +1,13 @@
+/**
+ * @version GO
+ * @author MARIO SAM <eu@mariosam.com.br>
+ * @see I would love to work with you instead solving web code tests: hire me!
+ */
+package GO
+
+/**
+ * Calculate worktime by timevalue.
+ */
+func calcSalary(v int, d float64) float64 {
+	return float64(v) * d
+}
