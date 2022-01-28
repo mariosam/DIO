f1b086e01dee6d6f5b1b697644b100cff93de1cd
Mario SAM <eu@mariosam.com.br>
2022-01-28 00:46:03 -0300

    Create VisitaNaFeira.go

---
+++ GO/VisitaNaFeira.go
@@ -0,0 +1,13 @@
+/**
+ * @version GO
+ * @author MARIO SAM <eu@mariosam.com.br>
+ * @see I would love to work with you instead solving web code tests: hire me!
+ */
+package GO
+
+/**
+ * Calculate the numbers of peppers.
+ */
+func totalPeppers(a int, b int) int {
+	return a + b
+}
