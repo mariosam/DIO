61bf59125c2352dcfae4f350be8df7d42c9bc0f0
Mario SAM <eu@mariosam.com.br>
2022-01-28 00:49:43 -0300

    Create VisitaNaFeira.php

---
+++ PHP/VisitaNaFeira.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @version PHP
+ * @author MARIO SAM <eu@mariosam.com.br>
+ * @see I would love to work with you instead solving web code tests: hire me!
+ */
+namespace PHP;
+
+//echo "The result is "+totalPeppers(8,8);
+
+class VisitaNaFeira {
+
+    /**
+     * Calculate the numbers of peppers.
+     */
+    public function totalPeppers($a, $b): int {
+        return $a + $b;
+    }
+
+}
