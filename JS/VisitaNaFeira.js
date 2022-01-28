a47e9d75a36cc47b40258a5165c73b26bc4ce6fe
Mario SAM <eu@mariosam.com.br>
2022-01-28 00:48:44 -0300

    Create VisitaNaFeira.js

---
+++ JS/VisitaNaFeira.js
@@ -0,0 +1,19 @@
+/**
+ * @version JAVASCRIPT
+ * @author MARIO SAM <eu@mariosam.com.br>
+ * @see I would love to work with you instead solving web code tests: hire me!
+ */
+
+let lines = gets().split("\n");
+var a = parseInt(lines[0]);
+var b = parseInt(lines[1]);
+
+print( totalPeppers(a, b) );
+
+/**
+ * Calculate the numbers of peppers.
+ */
+function totalPeppers(a, b) {
+    return a + b;
+}
+module.exports = {totalPeppers}
