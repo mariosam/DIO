7bcacfac3d5702ac999617d8256e92fddf23e05c
Mario SAM <eu@mariosam.com.br>
2022-02-01 01:56:23 -0300

    Create Tomadas.go

---
+++ GO/Tomadas.go
@@ -0,0 +1,13 @@
+/**
+ * @version GO
+ * @author MARIO SAM <eu@mariosam.com.br>
+ * @see I would love to work with you instead solving web code tests: hire me!
+ */
+package GO
+
+/**
+ * Calculate the number max of devices can be pluged.
+ */
+func maxDevices(t1 int, t2 int, t3 int, t4 int) int {
+	return (t1 + t2 + t3 + t4) - 3
+}
