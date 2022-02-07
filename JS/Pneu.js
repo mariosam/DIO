0fa74c4d217d3f08f14db668b0ea82ba325be1d3
Mario SAM <eu@mariosam.com.br>
2022-02-07 00:11:36 -0300

    Create Pneu.js

---
+++ JS/Pneu.js
@@ -0,0 +1,18 @@
+/**
+ * @version JAVASCRIPT
+ * @author MARIO SAM <eu@mariosam.com.br>
+ * @see I would love to work with you instead solving web code tests: hire me!
+ */
+
+let valor1 = parseInt(gets());
+let valor2 = parseInt(gets());
+
+print( difPression( valor1, valor2 ) );
+
+/**
+ * Calculate the difference pression.
+ */
+function difPression(x, y) {
+    return x - y;
+}
+module.exports = {difPression}
