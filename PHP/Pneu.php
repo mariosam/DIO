3780b8cda81c9899c2bdbbcf30939d1ff78d5fe8
Mario SAM <eu@mariosam.com.br>
2022-02-07 00:12:28 -0300

    Create Pneu.php

---
+++ PHP/Pneu.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @version PHP
+ * @author MARIO SAM <eu@mariosam.com.br>
+ * @see I would love to work with you instead solving web code tests: hire me!
+ */
+namespace PHP;
+
+//echo "The result is "+difPression(4, 4);
+
+class Pneu {
+
+    /**
+     * Calculate the difference pression.
+     */
+    public function difPression($x, $y): int {
+        return $x - $y;
+    }
+
+}
