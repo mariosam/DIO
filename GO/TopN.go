9057633ce5feebfc581f372f5b7fbac5dc233949
Mario SAM <eu@mariosam.com.br>
2022-02-10 00:12:29 -0300

    Create TopN.go

---
+++ GO/TopN.go
@@ -0,0 +1,37 @@
+/**
+ * @version GO
+ * @author MARIO SAM <eu@mariosam.com.br>
+ * @see I would love to work with you instead solving web code tests: hire me!
+ */
+package GO
+
+const TOP1 = "Top 1"
+const TOP3 = "Top 3"
+const TOP5 = "Top 5"
+const TOP10 = "Top 10"
+const TOP25 = "Top 25"
+const TOP50 = "Top 50"
+const TOP100 = "Top 100"
+
+/**
+ * Identify the range of position.
+ */
+func topRange(pos int) string {
+	var result string = TOP100
+
+	if pos < 2 {
+		result = TOP1
+	} else if pos < 4 {
+		result = TOP3
+	} else if pos < 6 {
+		result = TOP5
+	} else if pos < 11 {
+		result = TOP10
+	} else if pos < 26 {
+		result = TOP25
+	} else if pos < 51 {
+		result = TOP50
+	}
+
+	return result
+}
