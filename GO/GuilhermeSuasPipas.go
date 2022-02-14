fa5e52a30071565d99d4b067192ba380b45753a0
Mario SAM <eu@mariosam.com.br>
2022-02-14 00:37:49 -0300

    Create GuilhermeSuasPipas.go

---
+++ GO/GuilhermeSuasPipas.go
@@ -0,0 +1,13 @@
+/**
+ * @version GO
+ * @author MARIO SAM <eu@mariosam.com.br>
+ * @see I would love to work with you instead solving web code tests: hire me!
+ */
+package GO
+
+/**
+ * Realize how much twine is necessary.
+ */
+func numberOfTwine(n int) int {
+	return ((n * n) - (3 * n)) / 2
+}
