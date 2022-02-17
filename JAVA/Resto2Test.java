9829c643acb1c943c0a2aec4f10916370fd48a7a
Mario SAM <eu@mariosam.com.br>
2022-02-17 00:30:01 -0300

    Create Resto2Test.java

---
+++ JAVA/Resto2Test.java
@@ -0,0 +1,18 @@
+/** 
+ * @version JAVA
+ * @author MARIO SAM <eu@mariosam.com.br>
+ * @see I would love to work with you instead solving web code tests: hire me!
+ * $ mvn clean test -Dtest=your.package.TestClassName
+ */ 
+package JAVA;
+
+import org.junit.Test;
+
+public class Resto2Test {
+
+	@Test
+	public void testResto2() throws Exception {
+		Resto2.printAllRest2(34);
+	}
+
+}
