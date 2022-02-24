9bbcd7b4f873d60cdd66f588db721cc62c184120
Mario SAM <eu@mariosam.com.br>
2022-02-24 01:16:14 -0300

    Create TrianguloPoligonosRegulares.go

---
+++ GO/TrianguloPoligonosRegulares.go
@@ -0,0 +1,13 @@
+/**
+ * @version GO
+ * @author MARIO SAM <eu@mariosam.com.br>
+ * @see I would love to work with you instead solving web code tests: hire me!
+ */
+package GO
+
+/**
+ * Return the number of necessary triangules to compose a poligono.
+ */
+func minTriangules(x int) int {
+	return x - 2
+}
