e17dd7db2921f45eed1421519cb62207ea6ecc94
Mario SAM <eu@mariosam.com.br>
2022-02-26 02:14:30 -0300

    Create SeisNumerosImparesTest.java

---
+++ JAVA/SeisNumerosImparesTest.java
@@ -0,0 +1,19 @@
+/** 
+ * @version JAVA
+ * @author MARIO SAM <eu@mariosam.com.br>
+ * @see I would love to work with you instead solving web code tests: hire me!
+ * $ mvn clean test -Dtest=your.package.TestClassName
+ */ 
+package JAVA;
+
+import org.junit.Test;
+
+public class SeisNumerosImparesTest {
+
+    @Test
+    public void testSeisNumerosImpares() throws Exception {
+        SeisNumerosImpares.printSixOdd(8);
+        SeisNumerosImpares.printSixOdd(7);
+	  }
+
+}
