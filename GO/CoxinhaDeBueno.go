cc5e828fa99686490754454d38af40e8b9dac8d1
Mario SAM <eu@mariosam.com.br>
2022-03-24 01:19:01 -0300

    Create CoxinhaDeBueno.go

---
+++ GO/CoxinhaDeBueno.go
@@ -0,0 +1,15 @@
+/**
+ * @version GO
+ * @author MARIO SAM <eu@mariosam.com.br>
+ * @see I would love to work with you instead solving web code tests: hire me!
+ */
+package GO
+
+import "fmt"
+
+/**
+ * Do the math: DIV two values.
+ */
+func calcCoxinha(h float64, p float64) string {
+	return fmt.Sprintf("%.2f", h/p)
+}
