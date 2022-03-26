6ea09cb8d7b59b86133ee42c64d63f23417252a1
Mario SAM <eu@mariosam.com.br>
2022-03-26 00:41:54 -0300

    Create CoxinhaDeBueno.php

---
+++ PHP/CoxinhaDeBueno.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @version PHP
+ * @author MARIO SAM <eu@mariosam.com.br>
+ * @see I would love to work with you instead solving web code tests: hire me!
+ */
+namespace PHP;
+
+//echo "The result is "+calcCoxinha(8,8);
+
+class CoxinhaDeBueno {
+
+    /**
+     * Do the math: DIV two values.
+     */
+    public function calcCoxinha($h = 0, $p = 0) {
+        return round(($h / $p), 2);
+    }
+}
