e9c51b375cb8f3c08a22c57bb366dbd0f29ed5b2
Mario SAM <eu@mariosam.com.br>
2022-04-20 14:18:05 -0300

    Create SomaSimples.php

---
+++ PHP/SomaSimples.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @version PHP
+ * @author MARIO SAM <eu@mariosam.com.br>
+ * @see I would love to work with you instead solving web code tests: hire me!
+ */
+namespace PHP;
+
+//echo "The result is "+simpleSum(8,8);
+
+class SomaSimples {
+
+    /**
+     * Do the math: SUM two values.
+     */
+    public function simpleSum($a = 0, $b = 0): int {
+        return $a + $b;
+    }
+
+}
