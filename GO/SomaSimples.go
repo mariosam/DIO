3de2d35222d2c233da44940ee0935ee242cabc35
Mario SAM <eu@mariosam.com.br>
2022-04-23 04:19:21 -0300

    Create SomaSimples.go

---
+++ GO/SomaSimples.go
@@ -0,0 +1,13 @@
+/**
+ * @version GO
+ * @author MARIO SAM <eu@mariosam.com.br>
+ * @see I would love to work with you instead solving web code tests: hire me!
+ */
+package GO
+
+/**
+ * Do the math: SUM two values.
+ */
+func simpleSum(a int, b int) int {
+	return a + b
+}
