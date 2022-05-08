eb41278c21e71a47f2bb7639ba4a74e8214e0f37
Mario SAM <eu@mariosam.com.br>
2022-05-08 01:26:16 -0300

    Create Diferenca.php

---
+++ PHP/Diferenca.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @version PHP
+ * @author MARIO SAM <eu@mariosam.com.br>
+ * @see I would love to work with you instead solving web code tests: hire me!
+ */
+namespace PHP;
+
+//echo "The result is "+showDifference(5, 6, 7, 8);
+
+class Diferenca {
+
+    /**
+     * Do the Math to show the difference between the numbers.
+     */
+    public function showDifference($A, $B, $C, $D) {
+        return ($A * $B) - ($C * $D);
+    }
+
+}
