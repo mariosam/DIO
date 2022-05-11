209828a4647efb9d5b1217aebe0d3a813da8148e
Mario SAM <eu@mariosam.com.br>
2022-05-11 00:45:17 -0300

    Create Diferenca.go

---
+++ GO/Diferenca.go
@@ -0,0 +1,13 @@
+/**
+ * @version GO
+ * @author MARIO SAM <eu@mariosam.com.br>
+ * @see I would love to work with you instead solving web code tests: hire me!
+ */
+package GO
+
+/**
+ * Do the Math to show the difference between the numbers.
+ */
+func showDifference(A int, B int, C int, D int) int {
+	return (A * B) - (C * D)
+}
