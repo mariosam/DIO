5976ea71a060086ceec863379e381fec63de7369
Mario SAM <eu@mariosam.com.br>
2022-05-23 00:05:13 -0300

    Create SomaDeNumerosNaturais.go

---
+++ GO/SomaDeNumerosNaturais.go
@@ -0,0 +1,13 @@
+/**
+ * @version GO
+ * @author MARIO SAM <eu@mariosam.com.br>
+ * @see I would love to work with you instead solving web code tests: hire me!
+ */
+package GO
+
+/**
+ * Sum all natural numbers from A to B
+ */
+func sumAllNaturals(a int, b int) int {
+	return (b*(b+1))/2 - (a*(a-1))/2
+}
