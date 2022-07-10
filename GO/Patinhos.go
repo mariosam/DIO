b6f782ad15ce78f53f6e889919fec10992c0449c
Mario SAM <eu@mariosam.com.br>
2022-07-10 00:04:22 -0300

    Create Patinhos.go

---
+++ GO/Patinhos.go
@@ -0,0 +1,17 @@
+/**
+ * @version GO
+ * @author MARIO SAM <eu@mariosam.com.br>
+ * @see I would love to work with you instead solving web code tests: hire me!
+ */
+package GO
+
+/**
+ * Calculate how many ducks are returned.
+ */
+func returnedDucks(ducks int) int {
+	if ducks > 0 {
+		ducks--
+	}
+
+	return ducks
+}
