79f89a8544b1dc1e5073fcbc68e3e859fa2c1d06
Mario SAM <eu@mariosam.com.br>
2022-08-28 01:08:31 -0300

    Create Distancia.go

---
+++ GO/Distancia.go
@@ -0,0 +1,13 @@
+/**
+ * @version GO 1.19
+ * @author MARIO SAM <eu@mariosam.com.br>
+ * @see I would love to work with you instead solving web code tests: hire me!
+ */
+package GO
+
+/**
+ * Calculate the minutes based on the distance and the speed.
+ */
+func reportMinutes(km int, speed int) int {
+	return km * speed
+}
