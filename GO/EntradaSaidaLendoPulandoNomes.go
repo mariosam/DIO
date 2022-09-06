d2784e4e44f16c543f74801bc2799562cf77227b
Mario SAM <eu@mariosam.com.br>
2022-09-06 00:43:28 -0300

    Create EntradaSaidaLendoPulandoNomes.go

---
+++ GO/EntradaSaidaLendoPulandoNomes.go
@@ -0,0 +1,13 @@
+/**
+ * @version GO 1.19
+ * @author MARIO SAM <eu@mariosam.com.br>
+ * @see I would love to work with you instead solving web code tests: hire me!
+ */
+package GO
+
+/**
+ * Return the names of position 3, 7 and 9.
+ */
+func giveMe379(ar []string) []string {
+	return []string{ar[2], ar[6], ar[8]}
+}
