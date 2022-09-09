da8738d5a7229589180da166e67571b22d3281a2
Mario SAM <eu@mariosam.com.br>
2022-09-09 00:55:03 -0300

    Update CalculoSimples.java

--- JAVA/CalculoSimples.java
+++ JAVA/CalculoSimples.java
@@ -1,25 +1,31 @@
+/**
+ * @version JAVA
+ * @author MARIO SAM <eu@mariosam.com.br>
+ * @see I would love to work with you instead solving web code tests: hire me!
+ */
 package JAVA;
 
 import java.io.IOException;
 import java.util.Scanner;
 
 public class CalculoSimples {
-    
+
     public static void main(String[] args) throws IOException {
-        Scanner leitor = new Scanner(System.in);
-           
-         int cod1 = leitor.nextInt();
-         int n1 = leitor.nextInt();
-         double valor1 = leitor.nextDouble();
-           
-         int cod2 = leitor.nextInt();
-         int n2 = leitor.nextInt();
-         double valor2 = leitor.nextDouble();
-           
-   // TODO: Implemente um Cálculo Simples.
-   // Levando em consideração a sua precedência de operadores e a definição de suas casas decimais:
-           
-        double total = (n1 * valor1) + (n2 * valor2);
-        System.out.println(String.format("VALOR A PAGAR: R$ %.2f", total));   
+		Scanner sc = new Scanner(System.in);
+		String[] line1 = sc.nextLine().split(" ");
+		double[] p1 = { Double.parseDouble(line1[0]), Double.parseDouble(line1[1]), Double.parseDouble(line1[2]) };
+
+		String[] line2 = sc.nextLine().split(" ");
+		double[] p2 = { Double.parseDouble(line2[0]), Double.parseDouble(line2[1]), Double.parseDouble(line2[2]) };
+		sc.close();
+
+		System.out.println(String.format("VALOR A PAGAR: R$ %.2f", calcTotalToPay( p1, p2 ) ));
+	}
+
+    /**
+     * Multiply product value by quantity and return the total.
+     */
+    public static double calcTotalToPay(double[] pecas1, double[] pecas2) {
+        return (pecas1[1] * pecas1[2]) + (pecas2[1] * pecas2[2]);
     }
 }
