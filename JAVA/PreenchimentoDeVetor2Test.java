02e794f39fd6866281b183fc554ae99617b7f001
Mario SAM <eu@mariosam.com.br>
2022-09-14 01:05:02 -0300

    Create PreenchimentoDeVetor2Test.java

---
+++ JAVA/PreenchimentoDeVetor2Test.java
@@ -0,0 +1,18 @@
+/** 
+ * @version JAVA
+ * @author MARIO SAM <eu@mariosam.com.br>
+ * @see I would love to work with you instead solving web code tests: hire me!
+ * $ mvn clean test -Dtest=your.package.TestClassName
+ */ 
+package JAVA;
+
+import org.junit.Test;
+
+public class PreenchimentoDeVetor2Test {
+
+	@Test
+	public void testPreenchimentoDeVetor2() throws Exception {
+		PreenchimentoDeVetor2.printAllVector2(3);
+	}
+
+}
